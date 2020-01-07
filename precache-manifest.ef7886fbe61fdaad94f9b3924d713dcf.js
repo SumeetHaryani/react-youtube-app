7c4a62b713f02936374b4e4f6efba5be9ec3949b
SumeetHaryani <sumeetharyani3@gmail.com>
2020-01-07 22:49:00 +0530

    Updates

---
+++ precache-manifest.ef7886fbe61fdaad94f9b3924d713dcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd446db8c4d620ba17e09402d47d75cd",
+    "url": "/react-youtube-app/index.html"
+  },
+  {
+    "revision": "d92cfcf937cab18227f1",
+    "url": "/react-youtube-app/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d92cfcf937cab18227f1",
+    "url": "/react-youtube-app/static/js/2.b0b1cbda.chunk.js"
+  },
+  {
+    "revision": "744b76f766044157687ca8b61b891873",
+    "url": "/react-youtube-app/static/js/2.b0b1cbda.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eb04de9a3aa243f959d5",
+    "url": "/react-youtube-app/static/js/main.a1822617.chunk.js"
+  },
+  {
+    "revision": "8158f098873416ab114b",
+    "url": "/react-youtube-app/static/js/runtime-main.8c8f1bcc.js"
+  }
+]);
